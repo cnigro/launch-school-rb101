225069e14959a2be5d08f670b53d83b8da184385
Cameron Nigro <cameron.nigro@gmail.com>
2020-02-12 19:31:42 -0500

    answers to q11 - q15

---
+++ lesson_04/q12.rb
@@ -0,0 +1,15 @@
+# Add up all of the ages from the Munster family hash:
+
+ages = { "Herman" => 32, 
+         "Lily" => 30, 
+         "Grandpa" => 5843, 
+         "Eddie" => 10, 
+         "Marilyn" => 22, 
+         "Spot" => 237 }
+
+sum_of_ages = 0
+ages.each do |_, age|
+  sum_of_ages += value
+end
+
+sum_of_ages
