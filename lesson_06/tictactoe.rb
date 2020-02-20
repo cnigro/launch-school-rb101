1ba54a8924e285276294f7c1f273eb8ca451b882
Cameron Nigro <cameron.nigro@gmail.com>
2020-02-20 17:13:34 -0500

    add ability for player to mark the board

--- lesson_06/tictactoe.rb
+++ lesson_06/tictactoe.rb
@@ -1,3 +1,11 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def display_board(brd)
   puts ""
   puts "     |     |"
@@ -16,9 +24,28 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
+def available_squares(brd)
+  brd.keys.select { |n| brd[n] == INITIAL_MARKER }
+end
+
+def player_move!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{available_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if available_squares(brd).include?(square)
+    prompt 'Sorry, that\'s not a valid choice.'
+  end
+  brd[square] = PLAYER_MARKER
+end
+
 board = initialize_board
+display_board(board)
+
+player_move!(board)
+puts board.inspect
 display_board(board)
