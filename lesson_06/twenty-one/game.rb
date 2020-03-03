cbab7f74b9c006e3be7cd1e87078fa04e8ba3f79
Cameron Nigro <cameron.nigro@gmail.com>
2020-03-03 11:10:15 -0500

    refactor to please rubocop. ignoring method length offense

--- lesson_06/twenty-one/game.rb
+++ lesson_06/twenty-one/game.rb
@@ -9,13 +9,13 @@ def generate_deck
   suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
             'Jack', 'Queen', 'King', 'Ace']
-            
+
   deck = suits.map do |suit|
     values.map do |value|
       [suit, value]
     end
   end
-  deck = (deck[0] + deck[1] + deck[2] + deck[3]).shuffle
+  (deck[0] + deck[1] + deck[2] + deck[3]).shuffle
 end
 
 def deal_cards(deck, quantity)
@@ -26,27 +26,21 @@ def display_cards(table, final=false)
   system 'clear'
   player = table[:player]
   dealer = table[:dealer]
-  
   prompt("Player hand:")
   player.size.times do |n|
     puts "\t#{player[n][1]} of #{player[n][0]}"
   end
-  puts "\tTotal: #{total(table[:player])}"
+  puts "\tTotal: #{total(player)}"
 
   prompt("Dealer hand:")
-  # If this is the final display, we want to show
-  # the dealer's hidden card
-  # binding.pry
-  if final
-    # binding.pry
+  if final # Show the dealer's face-down card
     dealer.size.times do |n|
       puts "\t#{dealer[n][1]} of #{dealer[n][0]}"
     end
-    puts "\tTotal: #{total(table[:dealer])}"
+    puts "\tTotal: #{dealer}"
   else
-    # binding.pry
     (dealer.size - 1).times do |n|
-      puts "\t#{dealer[n+1][1]} of #{dealer[n+1][0]}"
+      puts "\t#{dealer[n + 1][1]} of #{dealer[n + 1][0]}"
     end
     puts "\tUnknown"
   end
@@ -54,22 +48,21 @@ end
 
 def total(cards)
   values = cards.map { |card| card[1] }
-  
   sum = 0
   values.each do |value|
-    if value == "Ace"
-      sum += 11
-    elsif ["Jack", "Queen", "King", "Ace"].include?(value)
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += case value
+           when "Ace"
+             11
+           when ["Jack", "Queen", "King", "Ace"].include?(value)
+             10
+           else
+             value.to_i
+           end
   end
 
   values.select { |value| value == 'Ace' }.count.times do
     sum -= 10 if sum > 21
   end
-  
   sum
 end
 
@@ -94,11 +87,11 @@ def initialize_game
   table
 end
 
-# Game Loop
 state = :player_turn
 table = initialize_game
 
-loop do 
+# Game Loop
+loop do
   case state
   when :begin
     table = initialize_game
@@ -137,7 +130,7 @@ loop do
     answer = gets.chomp
     state = :quit unless answer.casecmp?('y')
   when :dealer_turn
-    loop do 
+    loop do
       display_cards(table)
 
       if can_hit?(table[:dealer])
@@ -146,49 +139,35 @@ loop do
         if busted?(table[:dealer])
           state = :dealer_bust
           break
-        elsif won?(table[:dealer]) 
+        elsif won?(table[:dealer])
           state = :dealer_win
           break
         end
-      else 
+      else
         prompt("Dealer chose to stay!")
         state = :game_end
         break
       end
     end
   when :dealer_bust
-    prompt("Dealer busted!")
-    prompt("Final Hand")
     display_cards(table, true)
-    prompt("Would you like to play again?")
-    answer = gets.chomp
-    answer.casecmp?('y') ? state = :begin : state = :quit
+    prompt("Dealer busted!")
+    state = :game_end
   when :dealer_win
     display_cards(table, true)
     prompt("Dealer wins! Better luck next time!")
-    prompt("Would you like to play again?")
-    answer = gets.chomp
-    answer.casecmp?('y') ? state = :begin : state = :quit
+    state = :game_end
   when :game_end
-    prompt("Final Hand")
-    display_cards(table, true)
     if total(table[:dealer]) > total(table[:player])
       prompt("Dealer wins! Better luck next time!")
-      prompt("Would you like to play again?")
-      answer = gets.chomp
-      answer.casecmp?('y') ? state = :begin : state = :quit
     else
       prompt("You win!")
-      prompt("Would you like to play again?")
-      answer = gets.chomp
-      answer.casecmp?('y') ? state = :begin : state = :quit
     end
+    prompt("Would you like to play again?")
+    answer = gets.chomp
+    state = answer.casecmp?('y') ? :begin : :quit
   when :quit
     prompt("Thanks for playing 21! Goodbye!")
     break
   end
 end
-
-
-
-
